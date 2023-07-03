c61085b73c3781ed93dd863f1ccc2f437d0cb769
tastekim <tastekim@kakao.com>
2023-07-04 00:21:27 +0900

    :bulb: study hitURL

--- main.go
+++ main.go
@@ -1,26 +1,35 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"github.com/tastekim/learngo2/dict"
+	"net/http"
 )
 
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-	baseWord := "hello"
-	dictionary.Add(baseWord, "First")
-	dictionary.Search(baseWord)
-	dictionary.Delete(baseWord)
-	word, err := dictionary.Search(baseWord)
-	if err != nil {
-		fmt.Println(err)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	fmt.Println(word)
-	//account := accounts.NewAccount("tastekim")
-	//account.Deposit(10)
-	//err := account.WithDraw(20)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//fmt.Println(account.String())
+	for _, url := range urls {
+		hitURL(url)
+	}
+}
+
+var errRequestFailed = errors.New("Request failed.")
+
+func hitURL(url string) error {
+	fmt.Println("Checking : ", url)
+	resp, err := http.Get(url)
+	if err == nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
