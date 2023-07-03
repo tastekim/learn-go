a1b85fc6266ac6121950b5503fb02b4c3e370316
tastekim <tastekim@kakao.com>
2023-07-04 00:09:21 +0900

    :bulb: study Map2

--- dict/mydict.go
+++ dict/mydict.go
@@ -5,7 +5,9 @@ import "errors"
 // Dictionary type
 type Dictionary map[string]string
 
-var errNotFound = errors.New("Not found")
+var errNotFound = errors.New("Not found.")
+var errWordExists = errors.New("Word already exists.")
+var errCantUpdate = errors.New("Can't update non-existing word.")
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -14,4 +16,33 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+// Add a word
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case errWordExists:
+		return errWordExists
+	}
+	return nil
+}
+
+// Update a word
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = definition
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
+
+// Delete a word
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
--- main.go
+++ main.go
@@ -7,13 +7,15 @@ import (
 
 func main() {
 	dictionary := mydict.Dictionary{"first": "First word"}
-	dictionary["hello"] = "hello"
-	definition, err := dictionary.Search("firs")
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
 	}
+	fmt.Println(word)
 	//account := accounts.NewAccount("tastekim")
 	//account.Deposit(10)
 	//err := account.WithDraw(20)
