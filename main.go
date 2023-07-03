881742faf737d9932df5bd7bddbc07ad13dcd6d4
tastekim <tastekim@kakao.com>
2023-07-03 23:48:59 +0900

    :bulb: study Map1

--- main.go
+++ main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
-	"github.com/tastekim/learngo2/accounts"
+	"github.com/tastekim/learngo2/dict"
 )
 
 func main() {
-	account := accounts.NewAccount("tastekim")
-	account.Deposit(10)
-	err := account.WithDraw(20)
+	dictionary := mydict.Dictionary{"first": "First word"}
+	dictionary["hello"] = "hello"
+	definition, err := dictionary.Search("firs")
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println(definition)
 	}
-	fmt.Println(account.String())
+	//account := accounts.NewAccount("tastekim")
+	//account.Deposit(10)
+	//err := account.WithDraw(20)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//fmt.Println(account.String())
 }
